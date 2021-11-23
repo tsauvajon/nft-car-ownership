491a442c6fafa2a58512fc1f7379c2b5bd70725e
Thomas Sauvajon <thomas@sauvajon.tech>
2021-11-23 09:24:26 +0100

    Make progress with our contract + TypeScript

---
+++ remote-control/src/contract/car-nft.ts
@@ -0,0 +1,51 @@
+import Web3 from "web3";
+import { Contract, ContractSendMethod } from "web3-eth-contract";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+// Call a tx, and display an error if it fails.
+// Returns true if the call succeeds.
+async function tryCall(tx: ContractSendMethod): Promise<any> {
+    try {
+        return await tx.call();
+    } catch (e: any) {
+        console.error(e);
+        toast.error(e.message);
+    }
+}
+
+// Range returns an array of Numbers from 0 to length-1.
+// For example, range(3) === [0, 1, 2]
+function range(length: Number): Array<Number> {
+    return [...Array(length).keys()]
+}
+
+class CarNFT {
+    contract: Contract
+
+    constructor(contractInstance: Contract) {
+        this.contract = contractInstance
+    }
+
+    // Mint a new NFT, by ID.
+    async mint(account: string, tokenID: Number): Promise<void> {
+        const tx: ContractSendMethod = await this.contract.methods.mint(account, tokenID);
+        await tryCall(tx);
+    }
+
+    // Get all the NFTs of one owner.
+    async getNFTs(account: string): Promise<Array<Number> | undefined> {
+        const balanceTx: ContractSendMethod = await this.contract.methods.balanceOf(account);
+        const nftCount = await tryCall(balanceTx);
+
+        if (nftCount === undefined) { return; }
+
+        const ownedNFTs = await Promise.all(range(nftCount).map(async index => {
+            const nftTx = await this.contract.methods.tokenOfOwnerByIndex(account, index)
+            return await tryCall(nftTx);
+        }))
+
+        return ownedNFTs;
+    }
+}
--- remote-control/src/ethereum/register.ts
+++ remote-control/src/ethereum/register.ts
@@ -1,5 +1,14 @@
-import Web3 from 'web3'
-const CarNFT = require('../../../nft/build/contracts/CarNFT.json')
+import Web3 from "web3";
+const CarNFTContractMetadata = require("../../../nft/build/contracts/CarNFT.json");
+import { Contract } from "web3-eth-contract";
+
+declare global {
+    interface Window {
+        ethereum: any; // Injected by Metamask.
+        web3: Web3; // Injected by us to help with debugging.
+        carNFT: Contract; // Injected by us to help with debugging.
+    }
+}
 
 let register = () => new Promise(function (resolve, reject) {
     // Metamask injects its web3 instance into window.ethereum
@@ -22,21 +31,22 @@ let register = () => new Promise(function (resolve, reject) {
 
 async function getContract(web3: Web3) {
     if (!web3) {
-        throw new Error('missing parameter web3')
+        throw new Error("missing parameter web3")
     }
 
     // The contract needs to be deployed to the network we're logged in!
-    const network = web3.currentProvider.networkVersion
+    const network = web3.eth.net
+    const networkID = await network.getId()
 
     let address: string
     try {
-        address = CarNFT.networks[network].address
+        address = CarNFTContractMetadata.networks[networkID].address
     } catch (e) {
         console.error(e)
-        throw new Error('The contract is not deployed!')
+        throw new Error("The contract is not deployed!")
     }
 
-    const carNFT = new web3.eth.Contract(CarNFT.abi, address)
+    const carNFT = new web3.eth.Contract(CarNFTContractMetadata.abi, address)
     window.carNFT = carNFT
 
     return carNFT
