0823b0e45622eec26f23a6541ae767783536780b
Thomas Sauvajon <thomas@sauvajon.tech>
2021-11-30 20:30:17 +0100

    'open' and 'close' with lights

--- car/api/src/car_commands.rs
+++ car/api/src/car_commands.rs
@@ -1,7 +1,43 @@
 use crate::nft;
 use crate::pool::Pool;
+
 use actix::prelude::*;
 use actix_web::{web::Data, Responder};
+use hyper::{Client, StatusCode, Uri};
+
+const RED: u32 = 1000;
+const GREEN: u32 = 25000;
+
+pub async fn change_color(hue: u32) {
+    for id in 1..=2 {
+        set_light_color(hue, id).await;
+    }
+}
+
+pub async fn set_light_color(hue: u32, light_id: u32) {
+    let client = Client::new();
+    let uri = format!(
+        "http://192.168.1.33/api/9O2NMKebbLLjIY4Ekxhqa18-jP9hSTp7lp-oXzn1/lights/{:?}/state",
+        light_id
+    )
+    .parse::<Uri>()
+    .unwrap();
+
+    use hyper::{Body, Method, Request};
+    let req = Request::builder()
+        .method(Method::PUT)
+        .uri(uri)
+        .body(Body::from(format!(
+            "{{\"on\":true, \"sat\":254, \"bri\":254,\"hue\":{:?}}}",
+            hue
+        )))
+        .unwrap();
+
+    let resp = client.request(req).await.unwrap();
+    if resp.status() != StatusCode::OK {
+        println!("{:?}: {:?}", resp.status(), resp.body());
+    }
+}
 
 #[derive(Message)]
 #[rtype(result = "()")]
@@ -18,6 +54,8 @@ impl Handler<OpenTheCar> for Pool {
 pub async fn open(pool: Data<Addr<Pool>>) -> impl Responder {
     let car_owner = nft::get_car_owner().await.unwrap();
     println!("owner: {:?}", car_owner);
+
+    change_color(GREEN).await;
 
     pool.try_send(OpenTheCar {}).unwrap();
     "Opening the car!"
@@ -36,6 +74,8 @@ impl Handler<CloseTheCar> for Pool {
 }
 
 pub async fn close(pool: Data<Addr<Pool>>) -> impl Responder {
+    change_color(RED).await;
+
     pool.try_send(CloseTheCar {}).unwrap();
     "Closing the car!"
 }
